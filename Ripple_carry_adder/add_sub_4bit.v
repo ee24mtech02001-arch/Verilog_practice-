1935680190a9e5181c71c10bc414d63e02aca13a
Bhavani_2001 <ee24mtech02001@iith.ac.in>
2026-06-04 12:47:11 +0530

    Add testbench for 4-bit adder/subtractor module

--- Ripple_carry_adder/add_sub_4bit.v
+++ Ripple_carry_adder/add_sub_4bit.v
@@ -1,26 +1,28 @@
-module add_sub_4bit(
-    input [3:0] x,y,
-    input mode,
-    
-    output [3:0]sum,
-    output carry,
-    output     ovflow
-);
-    wire c1,c2,c3,cin,cout;
-    wire [3:0] y_xor;
+module ripple_add_tb;
+    reg  [3:0] a, b;
+    reg        m;
+    wire [3:0] s;
+    wire       c, ov;
 
-    assign y_xor = y ^{4{mode}};
-    assign cin= mode;
+    add_sub_4bit uut (
+        .x(a),
+        .y(b),
+        .mode(m),
+        .sum(s),
+        .carry(c),        
+        .ovflow(ov)
+    );
 
+    initial begin
+        $dumpfile("ripple_add.vcd");
+        $dumpvars(0, ripple_add_tb);
+        $monitor("mode=%b  a=%d  b=%d  =>  s=%d  carry=%b  ov=%b",
+                  m, a, b, s, c, ov);
 
-
-    full_add f0(sum[0],c1,x[0],y_xor[0],cin);
-    full_add f1(sum[1], c2, x[1],y_xor[1],c1);
-    full_add f2(sum[2], c3, x[2],y_xor[2],c2);
-    full_add f3(sum[3], cout, x[3],y_xor[3],c3);
-    assign carry = cout;
-    assign ovflow = c3^cout;
-    
-endmodule 
-
-    
+        #5 m=0; a=4'd5; b=4'd3;   // 5 + 3  = 8
+        #5 m=0; a=4'd7; b=4'd8;   // 7 + 8  = 15
+        #5 m=1; a=4'd5; b=4'd3;   // 5 - 3  = 2
+        #5 m=1; a=4'd7; b=4'd8;   // 7 - 8  = wraps to 15
+        #5 $finish;
+    end
+endmodule
